2b1db29d906ad601d9d56201974b2ee21c28c08f
sh990920 <sh990920@gmail.com>
2022-10-01 17:55:54 +0900

    Day09

---
+++ Day09/src/main/webapp/Ex1.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+for (var i = 2; i < 10; i++) {
+	document.write(i + "단" + "<br>");
+	for (var j = 1; j < 10; j++) {
+		document.write(i + " X " + j + " = " + i*j);
+		document.write("<br>");
+	}
+	document.write("<br>");
+}
+
+
+
